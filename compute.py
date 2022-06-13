d99857eaca8e1480a366c67b5ab297f8e54b1313
Sarah Jimenez <57870970+sarahmarie23@users.noreply.github.com>
2022-06-13 16:38:25 -0700

    Fixed checkyellow

--- compute.py
+++ compute.py
@@ -6,22 +6,22 @@ def compute():
     with open('sorted.txt') as myfile:
         words = myfile.read().splitlines()
 
-    greendict = {'s':3}
-    yellowlist = ['e']
-    #TODO: account for the position of yellow letters
-    greylist = ['t', 'a', 'i', 'r', 'c', 'l', 'u', 'd', 'm', 'y', ]
-    #TODO: account for guessing a double letter. We know s is at position 3 but there's no second s
+    greendict = {}
+    yellowdict = {'r': 1 , 'o' : 2, 'd' : 4}
+    greylist = ['a', 'i', 's', 'l', 'e', 'p', 'u',  ]
+    # TODO: account for guessing a double letter. Say the word is "basic" and you guess "bossy". There would be a greendict for s:2 but 
+    # the 2nd s can't be in greylist 
     for i in list(words):
         if checkgreen(i, greendict) is True:
             words.remove(i)
             
-        elif checkyellow(i, yellowlist) is False:
+        elif checkyellow(i, yellowdict) is True:
             words.remove(i)
             
         else: 
             if checkgrey(i, greylist) is True:
                 words.remove(i)
-    print('The possible words are:')
+    print('The {} possible words are:'.format(len(words)))
     pprint(words)
 
     myfile.close()
@@ -32,16 +32,24 @@ def checkgreen(i, greendict):
             return True
     return False
 
-def checkyellow(i, yellowlist):
-    if (all(letter in yellowlist for letter in i)):
-        return False
-    return True
+def checkyellow(i, yellowdict):
+    if not all(letter in i for letter in yellowdict.keys()):
+        return True
+    else:
+        for k, v in yellowdict.items():
+            if i[v] == k:
+                return True
+    return False
 
 def checkgrey(i, greylist):
     if any(letter in i for letter in greylist):
         return True
     return False
 
+        
+
+
+        
         
 
 
